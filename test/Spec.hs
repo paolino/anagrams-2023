678f268a6f669e6485cc6df54f29dbfda61d12af
paolino <paolo.veronelli@gmail.com>
2023-04-09 14:44:26 +0100

    Fix specs for trie module.

--- test/Spec.hs
+++ test/Spec.hs
@@ -3,30 +3,39 @@
 {-# LANGUAGE TypeApplications #-}
 {-# OPTIONS_GHC -Wno-incomplete-uni-patterns #-}
 
-import Control.Monad (forM_)
+import Data.Functor ((<&>))
+import Data.List (permutations, uncons)
+import Data.Maybe (mapMaybe)
 import Data.Set qualified as Set
-import Dict (dictionary)
 import Test.Hspec (describe, hspec, it)
 import Test.QuickCheck
     ( Gen
     , Testable (property)
-    , classify
+    , conjoin
     , elements
     , forAll
-    , label
     , listOf1
     , scale
     , shuffle
-    , tabulate
     , (===)
     )
 import Trie (collectPaths, load, unload)
 
+-- Generate a string with only 3 letters to get a better chance of
+-- superimposition of the prefixes when generating a dictionary.
+-- >>> import Test.QuickCheck
+-- >>> generate genWord
+-- "bbcbaabc"
 genWord :: Gen String
-genWord = listOf1 $ elements ['a' .. 'd']
+genWord = logScale' 1.2 $ listOf1 $ elements ['a' .. 'c']
 
+-- Generate a list of words with a logarithmic scale of length.
+-- long words are not very interesting.
+-- >>> import Test.QuickCheck
+-- >>> generate genDictionary
+-- ["ba","acbbacbbccbabc","ccaabccb","cbabbbca","cbbacab","aacbc","bccbbbabbba"]
 genDictionary :: Gen [String]
-genDictionary = logScale $ listOf1 genWord
+genDictionary = logScale' 1.3 $ listOf1 genWord
 
 main :: IO ()
 main = hspec $ do
@@ -48,26 +57,33 @@ main = hspec $ do
                         do \w -> (w, []) `elem` collectPaths consume w (load @Char xs)
                         do xs
         it "collect all words shuffled" $ do
-            forAll genDictionary
-                $ \xs -> property $ forM_ xs $ \w -> do
-                    w' <- shuffle w
-                    pure
-                        $ label (show w)
-                        $ (w, []) `elem` collectPaths consume w' (load @Char xs)
+            forAll
+                do
+                    -- we need to limit words to 6 letters, otherwise permutations
+                    -- will generate too many extractions
+                    ws <- fmap (take 6) <$> genDictionary
+                    ws' <- traverse shuffle ws
+                    pure $ zip ws ws'
+                do
+                    \xs ->
+                        conjoin $ xs <&> \(w, w') -> do
+                            (w, []) `elem` collectPaths consume w' (load @Char $ fst <$> xs)
 
 unique :: [[Char]] -> [[Char]]
 unique = Set.toList . Set.fromList
 
+-- this is overkill, but it's a good test, in reality we'd just need
+-- a list of all elements together with the rest of the list
+-- >>> consume "abc"
+-- [('a',"bc"),('b',"ac"),('c',"ba"),('b',"ca"),('c',"ab"),('a',"cb")]
 consume :: String -> [(Char, String)]
-consume [] = []
-consume (x : xs) = [(x, xs)]
+consume xs = mapMaybe uncons $ permutations xs
 
 -- | Adjust the QuickCheck @size@ parameter,
 -- by replacing it with its logarithm with respect to some given basis.
 logScale' :: Double -> Gen a -> Gen a
 logScale' b = scale (floor @Double . logBase b . fromIntegral . succ)
 
--- | Adjust the QuickCheck @size@ parameter,
--- by replacing it with its logarithm.
-logScale :: Gen a -> Gen a
-logScale = logScale' $ exp 1
+-- >>> import Test.QuickCheck
+-- >>> generate $ shuffle "abc"
+-- "abc"
