f74ddb0ab6afbaf67706c43e71810d6185b7a210
paolino <paolo.veronelli@gmail.com>
2023-04-09 09:21:11 +0100

    fix encoding of overlapping paths, add properties

--- src/Trie.hs
+++ src/Trie.hs
@@ -1,10 +1,8 @@
 {-# LANGUAGE ImportQualifiedPost #-}
 {-# LANGUAGE PatternSynonyms #-}
-{-# LANGUAGE ViewPatterns #-}
 
 module Trie
     ( Trie
-    , pattern Leaf
     , load
     , unload
     , singleton
@@ -17,24 +15,22 @@ import Data.Map.Strict (Map)
 import Data.Map.Strict qualified as Map
 
 -- | A trie is a recursive map from elements to tries.
-newtype Trie a = Trie {trieMap :: Map a (Trie a)} deriving (Eq, Show)
+data Trie a = Trie
+    { trieMap :: Map a (Trie a)
+    , isPath :: Bool
+    }
+    deriving (Eq, Show)
 
 instance Ord a => Semigroup (Trie a) where
-    Trie a <> Trie b = Trie $ Map.unionWith (<>) a b
+    Trie a ia <> Trie b ib = Trie (Map.unionWith (<>) a b) (ia || ib)
 
 instance Ord a => Monoid (Trie a) where
-    mempty = Trie Map.empty
-
--- | A pattern synonym for the empty trie.
-pattern Leaf :: Ord a => Trie a
-pattern Leaf <- (Map.null . trieMap -> True)
-    where
-        Leaf = mempty
+    mempty = Trie Map.empty False
 
 -- | A path of a trie.
 singleton :: Ord a => [a] -> Trie a
-singleton [] = Leaf
-singleton (x : xs) = Trie $ Map.singleton x $ singleton xs
+singleton [] = Trie Map.empty True
+singleton (x : xs) = Trie (Map.singleton x $ singleton xs) False
 
 -- | Normalize a list of lists into a trie.
 load :: Ord a => [[a]] -> Trie a
@@ -43,6 +39,10 @@ load = foldMap singleton
 -- | Denormalize a trie into a list of lists.
 unload :: Trie a -> [[a]]
 unload = destroy (pure . pure) $ \x y ys -> (x :) <$> y : ys
+
+-- | A pattern to match a path.
+pattern Path :: Trie a
+pattern Path <- Trie _ True
 
 -- | Destroy a trie.
 destroy
@@ -53,26 +53,36 @@ destroy
     -> Trie a
     -- ^ The trie to destruct.
     -> [b]
-destroy end cons (Trie m) = do
+destroy end cons (Trie m _) = do
     (c, t) <- Map.toList m
     case destroy end cons t of
         [] -> end c
-        y : ys -> cons c y ys
+        y : ys -> case t of 
+            Path -> end c <> cons c y ys
+            _ -> cons c y ys
 
 -- | A function to match paths on a trie. We are given a source of elements
 -- and a function to extract elements from the source. We return all possible
 -- paths made of extracted elements and their leftover seeds.
 collectPaths
     :: Ord a
-    => (b -> [(a, b)]) -- ^ Extractions from the source.
-    -> b -- ^ The source.
-    -> Trie a -- ^ The trie to traverse.
-    -> [([a], b)] -- ^ All possible paths and their leftover seeds.
+    => (b -> [(a, b)])
+    -- ^ Extractions from the source.
+    -> b
+    -- ^ The source.
+    -> Trie a
+    -- ^ The trie to traverse.
+    -> [([a], b)]
+    -- ^ All possible paths and their leftover seeds.
 collectPaths f = go []
   where
-    go acc source Leaf = [(reverse acc, source)]
-    go acc source (Trie t) = do
-        (y, source') <- f source
-        case Map.lookup y t of
-            Nothing -> []
-            Just t' -> go (y : acc) source' t'
+    go acc source trie@(Trie t _) =
+        case trie  of 
+            Path -> (reverse acc, source) : rest 
+            _ -> rest
+        where
+            rest = do
+                (x, source') <- f source
+                case Map.lookup x t of
+                    Nothing -> []
+                    Just t' -> go (x : acc) source' t'
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,73 @@
+{-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE ImportQualifiedPost #-}
+{-# LANGUAGE TypeApplications #-}
+{-# OPTIONS_GHC -Wno-incomplete-uni-patterns #-}
+
+import Control.Monad (forM_)
+import Data.Set qualified as Set
+import Dict (dictionary)
+import Test.Hspec (describe, hspec, it)
+import Test.QuickCheck
+    ( Gen
+    , Testable (property)
+    , classify
+    , elements
+    , forAll
+    , label
+    , listOf1
+    , scale
+    , shuffle
+    , tabulate
+    , (===)
+    )
+import Trie (collectPaths, load, unload)
+
+genWord :: Gen String
+genWord = listOf1 $ elements ['a' .. 'd']
+
+genDictionary :: Gen [String]
+genDictionary = logScale $ listOf1 genWord
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+    describe "respect invariants" $ do
+        it "load . unload == id" $ do
+            forAll genDictionary
+                $ \xs -> property $ unload (load @Char xs) === unique xs
+    describe "collectPaths" $ do
+        it "collect one word" $ do
+            forAll genDictionary
+                $ \xs -> property $ do
+                    w <- elements xs
+                    let rs = collectPaths consume w (load @Char xs)
+                    pure $ (w, []) `elem` rs
+        it "collect all words" $ do
+            forAll genDictionary
+                $ \xs -> property
+                    $ all
+                        do \w -> (w, []) `elem` collectPaths consume w (load @Char xs)
+                        do xs
+        it "collect all words shuffled" $ do
+            forAll genDictionary
+                $ \xs -> property $ forM_ xs $ \w -> do
+                    w' <- shuffle w
+                    pure
+                        $ label (show w)
+                        $ (w, []) `elem` collectPaths consume w' (load @Char xs)
+
+unique :: [[Char]] -> [[Char]]
+unique = Set.toList . Set.fromList
+
+consume :: String -> [(Char, String)]
+consume [] = []
+consume (x : xs) = [(x, xs)]
+
+-- | Adjust the QuickCheck @size@ parameter,
+-- by replacing it with its logarithm with respect to some given basis.
+logScale' :: Double -> Gen a -> Gen a
+logScale' b = scale (floor @Double . logBase b . fromIntegral . succ)
+
+-- | Adjust the QuickCheck @size@ parameter,
+-- by replacing it with its logarithm.
+logScale :: Gen a -> Gen a
+logScale = logScale' $ exp 1
